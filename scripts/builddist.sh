13766ff4673dca53b074bc740f5c4696d87177d8
Christoph Hauert <hauert@math.ubc.ca>
2024-02-10 16:59:27 -0800

    add gitexporter
    * create config file
    * exclude all historical paths (slims down public repo while preserving the commit history)
    * effectively starts history with major refactoring for maven
    * customized gitexporter to override existing symlinks

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -46,4 +46,12 @@ cp -a "$EVOLUDO_JRE_HOME"/target/EvoLudo.*.jar dist/ ;
 echo "Copying EvoLudo API documentation..." ;
 cp -a docs/api dist/
 
+if [ -f .settings/gitexporter.json ]; then
+    # updating public repository
+    echo "Updating public repository..." ;
+    npx gitexporter .settings/gitexporter.json
+    # delete all empty directories - name of public repo must match settings in gitexporter.json
+    find ../EvoLudoTrial -type d -not \( -path "*/.git/*" -o -path "*/target/*" \) -empty -delete
+fi
+
 echo "Building EvoLudo distribution done!" ;
