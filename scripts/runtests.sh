b5cf984acc868b6a6ea9ced7842f5a4726a8fe70
Christoph Hauert <hauert@math.ubc.ca>
2024-08-12 13:06:29 -0700

    apply clo revisions to tests
    * use generator files for testing (more tolerant when changing clo names)

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -4,6 +4,7 @@ EVOLUDO_TEST_HOME="EvoLudoTest" ;
 EVOLUDO_TEST_TEST="$EVOLUDO_TEST_HOME/tests" ;
 
 java -jar "$EVOLUDO_TEST_HOME"/target/EvoLudoTest.*.jar \
+    	--tests "$EVOLUDO_TEST_TEST/generators" \
     	--references "$EVOLUDO_TEST_TEST/references/current" \
 		--reports "$EVOLUDO_TEST_TEST/reports" \
 		"$@"
