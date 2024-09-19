59649668921f57753e9bf1273bd614002b1e2aa4
Christoph Hauert <hauert@math.ubc.ca>
2024-09-19 14:37:43 +0200

    onResize not necessary on activation

--- EvoLudoGWT/src/main/java/org/evoludo/graphics/AbstractGraph.java
+++ EvoLudoGWT/src/main/java/org/evoludo/graphics/AbstractGraph.java
@@ -543,7 +543,6 @@ public abstract class AbstractGraph<B> extends FocusPanel
 	 */
 	public void activate() {
 		isActive = true;
-		onResize();
 		if (this instanceof Zooming) {
 			boolean isEPub = NativeJS.isEPub();
 			// important: ibooks (desktop) returns ePubReader for standalone pages as well,
