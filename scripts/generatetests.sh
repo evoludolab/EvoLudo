8500999719512f3ddd752069f33fe12c21af01bb
Christoph Hauert <hauert@math.ubc.ca>
2025-02-26 18:49:36 +0100

    improve test generator script

--- scripts/generatetests.sh
+++ scripts/generatetests.sh
@@ -15,26 +15,24 @@ EVOLUDO_SH="scripts"
 LATEST_JAVA=`find . -type f -name '*.java' -print0 | \
 			xargs -0 stat -f "%m %N" | \
 			sort -rn | head -1 | cut -f2- -d" "` ;
-LATEST_GWT=`find ${EVOLUDO_GWT_HOME}/target/EvoLudoGWT* \
-			-type f -name 'evoludoweb.nocache.js'` ;
-LATEST_JAR=`find ${EVOLUDO_JRE_HOME}/target/ \
-			-type f -name 'EvoLudo.*.jar' -print0 | \
+LATEST_TEST_JAR=`find ${EVOLUDO_TEST_HOME}/target/ \
+			-type f -name 'EvoLudo*.jar' -print0 | \
 			xargs -0 stat -f "%m %N" | \
 			sort -rn | head -1 | cut -f2- -d" "` ;
 
 # build project - if needed
-if [[ ${LATEST_JAVA} -nt ${LATEST_GWT} || ${LATEST_JAVA} -nt ${LATEST_JAR} ]]; then
-	echo "Building EvoLudo project..." ;
-	mvn clean install ;
+if [[ ${LATEST_JAVA} -nt ${LATEST_TEST_JAR} ]]; then
+	echo "Building EvoLudo test suite..." ;
+	mvn -pl EvoLudoTest clean install ;
 else
-	echo "Building EvoLudo project skipped..."
+	echo "EvoLudo test suite up to date."
 fi
 
 passed=$? ;
 
 if [ $passed -ne 0 ]
 	then 
-	echo "Building EvoLudo project failed - aborting!" ;
+	echo "Building EvoLudo test suite failed - aborting!" ;
 	exit $passed ;
 fi
 
@@ -64,9 +62,6 @@ if (( $nreportsbefore == $nreportsafter ))
 		# find newest directory - must be the one generated for the set of tests
 		NEWSET=$(ls -td ./*/ | head -1) ;
 		echo "All tests for set '$NEWSET' successfully generated." ;
-		echo "Setting current reference to new set of tests." ;
-		rm ./current ;
-		ln -s "$NEWSET" current ;
 		popd > /dev/null ;
 	else
 		echo "Some tests had issues." ;
