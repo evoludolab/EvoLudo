f7534e30abb8b247b03bac7209bd4b6aa354fe41
Christoph Hauert <hauert@math.ubc.ca>
2025-07-26 00:28:41 -0700

    add draft package documentation
    * include overview with logo

---
+++ EvoLudoGWT/src/main/java/org/evoludo/simulator/resources/package-info.java
@@ -0,0 +1,34 @@
+//
+// EvoLudo Project
+//
+// Copyright 2010-2025 Christoph Hauert
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// For publications in any form, you are kindly requested to attribute the
+// author and project as follows:
+//
+//	Hauert, Christoph (<year>) EvoLudo Project, https://www.evoludo.org
+//			(doi: 10.5281/zenodo.14591549 [, <version>])
+//
+//	<year>:    year of release (or download), and
+//	<version>: optional version number (as reported in output header
+//			or GUI console) to simplify replication of reported results.
+//
+// The formatting may be adjusted to comply with publisher requirements.
+//
+
+/**
+ * The GWT specific resources.
+ */
+package org.evoludo.simulator.resources;
